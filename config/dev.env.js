85c7abaa58da58bac8aafa65feef136b38e662e3
Elsa Dou <Elsa.Dou@CDP.COM>
2019-01-16 17:06:11 +0800

    demo 路径

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_HOST: '"https://dev-web.ecosaas.com"'
+  API_HOST: '"https://dev-swagger.ecosaas.com/ecosaasci"'  
 })
