d4756158657e0608b85cf245006f5a1ae864e51c
Elsa Dou <Elsa.Dou@CDP.COM>
2019-01-17 16:10:56 +0800

    后端改动

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_HOST: '"https://dev-swagger.ecosaas.com/ecosaasci"'  
+ // API_HOST: '"https://dev-swagger.ecosaas.com/ecosaasci"'  
+ API_HOST: '"https://dev-web.ecosaas.com"'
 })
